9ab4c0533c2ad860ef8a96336fd52dd3c7c635c9
munchycool <munchy.cool@gmail.com>
2018-05-07 18:03:50 +0000

    May 2018 Build Fingerprint

--- carbon_walleye.mk
+++ carbon_walleye.mk
@@ -46,5 +46,6 @@ PRODUCT_MANUFACTURER := Google
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=walleye \
-    BUILD_FINGERPRINT=google/walleye/walleye:8.1.0/OPM2.171019.029/4657601:user/release-keys \
-    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM2.171019.029 4657601 release-keys"
+    BUILD_FINGERPRINT=google/walleye/walleye:8.1.0/OPM4.171019.016.B1/4720843:user/release-keys \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM4.171019.016.B1 4720843 release-keys"
+
