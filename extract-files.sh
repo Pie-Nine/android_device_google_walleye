a1e4b3e2633a5421ddbd30ae748c02fdeb8dd477
void(0) <munchycool@users.noreply.github.com>
2018-05-08 17:36:17 -0400

    try carbon extract files

--- extract-files.sh
+++ extract-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -25,40 +25,12 @@ if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
 
 LINEAGE_ROOT="$MY_DIR"/../../..
 
-HELPER="$LINEAGE_ROOT"/vendor/lineage/build/tools/extract_utils.sh
+HELPER="$LINEAGE_ROOT"/vendor/carbon/build/tools/extract_utils.sh
 if [ ! -f "$HELPER" ]; then
     echo "Unable to find helper script at $HELPER"
     exit 1
 fi
 . "$HELPER"
-
-# Default to sanitizing the vendor folder before extraction
-#CLEAN_VENDOR=false
-
-
-#while [ "$1" != "" ]; do
-#    case $1 in
-#        -n | --no-cleanup )     CLEAN_VENDOR=false
-#                                ;;
-#        -s | --section )        shift
-#                                SECTION=$1
-#                                CLEAN_VENDOR=false
-#                                ;;
-#        * )                     SRC=$1
-#                                ;;
-#    esac
-#    shift
-#done
-
-#if [ -z "$SRC" ]; then
-#    SRC=adb
-#fi
-
-# Initialize the helper
-#setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false "$CLEAN_VENDOR"
-
-# Initialize the helper
-#setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT" false false
 
 if [ $# -eq 0 ]; then
   SRC=adb
@@ -78,7 +50,6 @@ fi
 
 # Initialize the helper
 setup_vendor "$DEVICE" "$VENDOR" "$LINEAGE_ROOT"
-
 
 extract "$MY_DIR"/device-proprietary-files.txt "$SRC"
 
