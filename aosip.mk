b441c538bdf74034f88a9de333a30e4d429e1bb5
munchy <munchycool@users.noreply.github.com>
2018-05-14 19:25:10 -0400

    add bootanimation details

--- aosip.mk
+++ aosip.mk
@@ -12,7 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit AOSiP GSM telephony parts
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit AOSiP GSM product configuration
 $(call inherit-product, vendor/aosip/config/common_full_phone.mk)
 
 # Inherit from the common Open Source product configuration
