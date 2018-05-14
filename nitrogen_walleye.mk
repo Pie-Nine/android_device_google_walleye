c786419b1621e475fe2ff4355907319034c15c3b
munchy <munchycool@users.noreply.github.com>
2018-05-14 19:24:20 -0400

    add bootanimation details

--- nitrogen_walleye.mk
+++ nitrogen_walleye.mk
@@ -12,13 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit Carbon GSM telephony parts
-#$(call inherit-product, vendor/carbon/config/gsm.mk)
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit Nitrogen OS product configuration
 $(call inherit-product, vendor/nitrogen/products/common.mk)
-
-# Inherit Carbon product configuration
-#$(call inherit-product, vendor/carbon/config/common.mk)
-
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
