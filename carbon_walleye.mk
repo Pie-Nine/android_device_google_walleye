7d71226fc4fc10165f2d92a3af50c75523089fe0
munchycool <munchy.cool@gmail.com>
2018-06-05 19:38:42 +0000

    walleye : June 2018 fingerprint

--- carbon_walleye.mk
+++ carbon_walleye.mk
@@ -12,11 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 1920
-TARGET_SCREEN_WIDTH := 1080
-
-
 # Inherit Carbon GSM telephony parts
 $(call inherit-product, vendor/carbon/config/gsm.mk)
 
@@ -51,6 +46,5 @@ PRODUCT_MANUFACTURER := Google
 # Device Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=walleye \
-    BUILD_FINGERPRINT=google/walleye/walleye:8.1.0/OPM2.171019.029.B1/4720900:user/release-keys \
-    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM2.171019.029.B1 4720900 release-keys"
-
+    BUILD_FINGERPRINT=google/walleye/walleye:8.1.0/OPM2.171026.006.C1/4769658:user/release-keys \
+    PRIVATE_BUILD_DESC="walleye-user 8.1.0 OPM2.171026.006.C1 4769658 release-keys"
