52186eb3c0add63b7533cdd645ff5ad825e8f917
munchycool <munchy.cool@gmail.com>
2018-07-19 14:59:39 +0000

    walleye : remove kernel props already in wahoo tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,11 +21,11 @@ include device/google/wahoo/BoardConfig.mk
 -include vendor/google/walleye/BoardConfigVendor.mk
 
 # Inline kernel building
-TARGET_KERNEL_SOURCE := kernel/google/wahoo
-BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
-TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_NEEDS_DTBOIMAGE := true
-TARGET_KERNEL_CONFIG := munchy_defconfig
+#TARGET_KERNEL_SOURCE := kernel/google/wahoo
+#BOARD_KERNEL_IMAGE_NAME := Image.lz4-dtb
+#TARGET_KERNEL_CLANG_COMPILE := true
+#TARGET_NEEDS_DTBOIMAGE := true
+#TARGET_KERNEL_CONFIG := munchy_defconfig
 BOARD_BOOTIMAGE_PARTITION_SIZE := 33554432
 
 #sepolicy common to muskie/walleye
